3f3563b1d8e92a1e85dd84c8135b09a434ce3857
ealpha072 <ealpha072@gmail.com>
2023-11-15 21:35:22 +0300

    Minor changes

--- client/src/utilities.js
+++ client/src/utilities.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 
 
+
+console.log(__dirname)
+
 fs.readdir('C:\\Users\\Admin\\Desktop\\Github\\electron-app\\client\\src\\backend', (err, files) => {
     console.log(files);
 })
